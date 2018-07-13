4fdb8b56f3c1736ae605a4697c83d6c0fe8ff615
Djamel Bourokba <jam@Djamels-MacBook-Pro.local>
2018-07-13 15:28:32 +0200

    update openshift 3.7

--- pre-reqs/2_install-openshift.sh
+++ pre-reqs/2_install-openshift.sh
@@ -4,8 +4,8 @@ red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
 
-#oc_download_url="https://github.com/openshift/origin/releases/download/v3.7.0/openshift-origin-client-tools-v3.7.0-7ed6862-linux-64bit.tar.gz"
-oc_download_url="https://github.com/openshift/origin/releases/download/v3.6.1/openshift-origin-client-tools-v3.6.1-008f2d5-linux-64bit.tar.gz" 
+oc_download_url="https://github.com/openshift/origin/releases/download/v3.7.0/openshift-origin-client-tools-v3.7.0-7ed6862-linux-64bit.tar.gz"
+#oc_download_url="https://github.com/openshift/origin/releases/download/v3.6.1/openshift-origin-client-tools-v3.6.1-008f2d5-linux-64bit.tar.gz" 
 
 
 echo "${red}Download then extract tar and cp oc tool${reset}"
